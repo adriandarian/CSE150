7387c558cdb9af14008dcba9b30cefa6a901254a
Adrian Darian <adarian@ucmerced.edu>
2018-10-05 18:50:34 -0700

    updated Lint

--- nachos/threads/LotteryScheduler.java
+++ nachos/threads/LotteryScheduler.java
@@ -23,26 +23,25 @@ import java.util.Iterator;
  * <p>
  * A lottery scheduler must partially solve the priority inversion problem; in
  * particular, tickets must be transferred through locks, and through joins.
- * Unlike a priority scheduler, these tickets add (as opposed to just taking
- * the maximum).
+ * Unlike a priority scheduler, these tickets add (as opposed to just taking the
+ * maximum).
  */
 public class LotteryScheduler extends PriorityScheduler {
-    /**
-     * Allocate a new lottery scheduler.
-     */
-    public LotteryScheduler() {
-    }
-    
-    /**
-     * Allocate a new lottery thread queue.
-     *
-     * @param	transferPriority	<tt>true</tt> if this queue should
-     *					transfer tickets from waiting threads
-     *					to the owning thread.
-     * @return	a new lottery thread queue.
-     */
-    public ThreadQueue newThreadQueue(boolean transferPriority) {
-	// implement me
-	return null;
-    }
+  /**
+   * Allocate a new lottery scheduler.
+   */
+  public LotteryScheduler() {
+  }
+
+  /**
+   * Allocate a new lottery thread queue.
+   *
+   * @param transferPriority <tt>true</tt> if this queue should transfer tickets
+   *                         from waiting threads to the owning thread.
+   * @return a new lottery thread queue.
+   */
+  public ThreadQueue newThreadQueue(boolean transferPriority) {
+    // implement me
+    return null;
+  }
 }
