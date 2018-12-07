77745240aff059e11914381c8f64d75892724f59
Adrian Darian <adarian@ucmerced.edu>
2018-12-07 00:36:12 -0800

    kernel

--- nachos/threads/ThreadedKernel.java
+++ nachos/threads/ThreadedKernel.java
@@ -2,27 +2,15 @@ package nachos.threads;
 
 import nachos.machine.*;
 
-/**
- * A multi-threaded OS kernel.
- */
 public class ThreadedKernel extends Kernel {
-  /**
-   * Allocate a new multi-threaded kernel.
-   */
+
   public ThreadedKernel() {
     super();
   }
 
-  /**
-   * Initialize this kernel. Creates a scheduler, the first thread, and an alarm,
-   * and enables interrupts. Creates a file system if necessary.
-   */
   public void initialize(String[] args) {
-    // set scheduler
     String schedulerName = Config.getString("ThreadedKernel.scheduler");
     scheduler = (Scheduler) Lib.constructObject(schedulerName);
-
-    // set fileSystem
     String fileSystemName = Config.getString("ThreadedKernel.fileSystem");
     if (fileSystemName != null)
       fileSystem = (FileSystem) Lib.constructObject(fileSystemName);
@@ -30,13 +18,8 @@ public class ThreadedKernel extends Kernel {
       fileSystem = Machine.stubFileSystem();
     else
       fileSystem = null;
-
-    // start threading
     new KThread(null);
-
     alarm = new Alarm();
-
-    Machine.interrupt().enable();
   }
 
   /**
@@ -46,35 +29,18 @@ public class ThreadedKernel extends Kernel {
    * here.
    */
   public void selfTest() {
-    KThread.selfTest();
-    Semaphore.selfTest();
-    SynchList.selfTest();
-    if (Machine.bank() != null) {
-      ElevatorBank.selfTest();
-    }
   }
 
-  /**
-   * A threaded kernel does not run user programs, so this method does nothing.
-   */
   public void run() {
   }
 
-  /**
-   * Terminate this kernel. Never returns.
-   */
   public void terminate() {
     Machine.halt();
   }
 
-  /** Globally accessible reference to the scheduler. */
   public static Scheduler scheduler = null;
-  /** Globally accessible reference to the alarm. */
   public static Alarm alarm = null;
-  /** Globally accessible reference to the file system. */
   public static FileSystem fileSystem = null;
-
-  // dummy variables to make javac smarter
   private static RoundRobinScheduler dummy1 = null;
   private static PriorityScheduler dummy2 = null;
   private static LotteryScheduler dummy3 = null;
